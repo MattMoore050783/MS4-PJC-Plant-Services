f7a312b5ea47e56133bc65dd359800b84ca756d3
Matt Moore <58918583+MattMoore050783@users.noreply.github.com>
2021-06-25 11:47:55 +0100

    deploy to heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn pjc_plant_services_ms4.wsgi:application
--- pjc_plant_services_ms4/settings.py
+++ pjc_plant_services_ms4/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '=s$m+w9d3wf&^-npls7qo50xchk_fsyfs14zw6j#kee+_w0qh!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['pjc-plant-services-ms4.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -120,13 +120,17 @@ WSGI_APPLICATION = 'pjc_plant_services_ms4.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
- 'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-   }
-}
-
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
