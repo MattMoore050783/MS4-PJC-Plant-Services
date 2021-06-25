d9b2249b25ea2b3a959f6c20e63149c7301f8640
Matt Moore <58918583+MattMoore050783@users.noreply.github.com>
2021-06-25 13:15:58 +0100

    REMOVE SECRET KEY AND DEBUG

--- pjc_plant_services_ms4/settings.py
+++ pjc_plant_services_ms4/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=s$m+w9d3wf&^-npls7qo50xchk_fsyfs14zw6j#kee+_w0qh!'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['pjc-plant-services-ms4.herokuapp.com', 'localhost']
 
